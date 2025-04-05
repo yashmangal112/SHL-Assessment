ae056ed11be7abdaf9244824806909397c327f6f
Yash <yashmangal240@gmail.com>
2025-04-05 15:52:44 +0530

    fixed some issues

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# main.py
+import os
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from suggest import suggest
@@ -16,3 +16,8 @@ app.add_middleware(
 @app.get("/suggest")
 def get_recommendations(query: str = Query(..., min_length=10)):
     return suggest(query)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # fallback to 8000 if PORT not set
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
